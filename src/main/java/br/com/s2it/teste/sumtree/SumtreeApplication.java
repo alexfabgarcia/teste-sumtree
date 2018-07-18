39b5904bdc82cae8b842b54384e93f422a0db8f6
Alex Garcia <agarcia46@dxc.com>
2018-07-18 16:28:29 -0300

    Estrutura inicial do projeto, criada através do Spring Initializer.

---
+++ src/main/java/br/com/s2it/teste/sumtree/SumtreeApplication.java
@@ -0,0 +1,12 @@
+package br.com.s2it.teste.sumtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumtreeApplication.class, args);
+	}
+}
